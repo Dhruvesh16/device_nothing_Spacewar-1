01ef7e50ca38557fead6e3bc5ca2933434a3e766
dhruvesh16 <dhruvesh3466@protonmail.com>
2022-11-16 20:20:58 +0530

    Spacewar: Sparkify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Spacewar.mk
+    $(LOCAL_DIR)/spark_Spacewar.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Spacewar-userdebug \
-    lineage_Spacewar-user \
-    lineage_Spacewar-eng
+    spark_Spacewar-userdebug \
+    spark_Spacewar-user \
+    spark_Spacewar-eng
