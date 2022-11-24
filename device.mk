4dab2b351732ee0a6d0510010806b1bc02ea38fe
dhruvesh16 <dhruvesh3466@protonmail.com>
2022-11-24 16:30:33 +0530

    Spacewar: Fix typo!

--- device.mk
+++ device.mk
@@ -186,7 +186,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths_shimaqrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths_shimaqrd.xml \
     $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths_yupikidp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths_yupikidp.xml \
     $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths_yupikqrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths_yupikqrd.xml \
-    $(LOCAL_PATH)/configs/audio/sound_trigger_platform__info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform__info.xml
+    $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform__info.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
