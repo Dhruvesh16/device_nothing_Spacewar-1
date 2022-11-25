75fba82a1583507ad799f13ab6546213cb2c3bd3
dhruvesh16 <dhruvesh3466@protonmail.com>
2022-11-25 15:46:17 +0530

    Spacewar: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/Dhruvesh16/vendor_nothing_Spacewar.git -b wip vendor/nothing/Spacewar
+git clone https://github.com/Dhruvesh16/kernel_nothing_sm7325.git kernel/nothing/sm7325
